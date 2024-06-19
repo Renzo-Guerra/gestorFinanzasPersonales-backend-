89a9633ca4f7acb31c3660b8982f2babc6edfef1
renzo-guerra <renzoguerralatt@gmail.com>
2024-06-19 12:22:32 -0300

    Manejo de errores de @Valid
    Al intentar crear un DTO o una entidad, si algun campo no cumple con las
    validaciones correspondientes se delverá un objeto con los errores.

--- src/main/java/com/gestionFinanzasPersonales/gestionFinanzasPersonales/Exceptions/ExceptionsHandler.java
+++ src/main/java/com/gestionFinanzasPersonales/gestionFinanzasPersonales/Exceptions/ExceptionsHandler.java
@@ -3,9 +3,10 @@ package com.gestionFinanzasPersonales.gestionFinanzasPersonales.Exceptions;
 import com.gestionFinanzasPersonales.gestionFinanzasPersonales.Exceptions.Exceptions.EmailYaExisteException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
+import org.springframework.validation.FieldError;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,5 +19,19 @@ public class ExceptionsHandler {
         errorResponse.put("error", exception.getMessage());
 
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    // Se ejecuta cuando el @Valid de los DTO/Entidades no se cumple
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException exceptions) {
+        Map<String, String> errors = new HashMap<>();
+        // Obtenemos todas las excepciones del BindingResult, y agregamos cada una nuestro Map
+        exceptions.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError)error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
